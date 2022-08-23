69437cfffa88560054c32249dc02efb549a443a7
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-08-23 16:07:16 +1000

    Write route for updating an existing blog post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -12,6 +12,7 @@ const withAuth = require('../../utils/auth');
 // Create new blog post
 router.post('/', withAuth, async (req, res) => {
 	try {
+		// Create new post based on request information
 		const newPost = await Post.create({
 			...req.body,
 			user_id: req.session.user_id,
@@ -25,6 +26,27 @@ router.post('/', withAuth, async (req, res) => {
 });
 
 // Update blog post
+router.put('/:id', async (req, res) => {
+	try {
+		// Find and update post based on id and request information
+		const postData = await Post.update(req.body, {
+			where: {
+				id: req.params.id,
+			},
+		});
+
+		// Error if no post with id matching the one in the request
+		if (!postData) {
+			res.status(404).json({ message: 'No post found with that id.' });
+			return;
+		}
+
+		res.status(200).json(postData);
+	} catch (err) {
+		console.log(err);
+		res.status(400).json(err);
+	}
+});
 
 // Delete blog post
 
