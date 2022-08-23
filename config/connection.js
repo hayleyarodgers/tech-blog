3aa68b666cf1e0c7ef4e1f7acffab112ba80561c
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-08-23 21:38:27 +1000

    Add improved comments

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,17 @@
 // Import Sequelize for working with mySQL database more easily
 const Sequelize = require('sequelize');
 
-// Load environmental variables from .env file into process.env to connect with mySQL database with hidden credentials
+// Load environmental variables from .env file into process.env to connect with database on localhost with hidden credentials
 require('dotenv').config();
 
 // Create a Sequelize instance to connect to the database
-// If there is an existing JAWSDB server ... oherwise, create a connection ... ?
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+	// If application is running on Heroku, connect to the database using the JAWSDB server
 	sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+	// If application is running on localhost, connect to the database in alternative way
 	sequelize = new Sequelize(
 		process.env.DB_NAME,
 		process.env.DB_USER,
