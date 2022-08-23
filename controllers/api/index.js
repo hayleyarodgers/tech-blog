91b4ce151f6df80fbcd46a0ac5171c91803966c2
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-08-23 15:27:27 +1000

    Write index.js for specifying different api routes

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+const userRoutes = require('./userRoutes');
+router.use('/users', userRoutes);
+
+const postRoutes = require('./postRoutes');
+router.use('/posts', postRoutes);
+
+const commentRoutes = require('./commentRoutes');
+router.use('/comments', commentRoutes);
+
+module.exports = router;
