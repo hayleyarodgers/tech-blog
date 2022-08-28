4a0eb457314760883dd51dde3eaec2be0b6d3488
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-08-28 13:03:05 +1000

    Write js for signing up

---
+++ public/js/signup.js
@@ -0,0 +1,25 @@
+const signupFormHandler = async (event) => {
+	event.preventDefault();
+
+	const username = document.querySelector('#signup-username').value.trim();
+	const email = document.querySelector('#signup-email').value.trim();
+	const password = document.querySelector('#signup-password').value.trim();
+
+	if (username && email && password) {
+		const response = await fetch('/api/user', {
+			method: 'POST',
+			body: JSON.stringify({ username, email, password }),
+			headers: { 'Content-Type': 'application/json' },
+		});
+
+		if (response.ok) {
+			document.location.replace('/dashboard');
+		} else {
+			alert(response.statusText);
+		}
+	}
+};
+
+document
+	.querySelector('#signup-button')
+	.addEventListener('click', signupFormHandler);
