c4b2563b663c274b33937166159424f604ab4954
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-08-28 14:10:40 +1000

    Write js for adding a new post

---
+++ public/js/addPost.js
@@ -0,0 +1,20 @@
+const addPost = async (event) => {
+	event.preventDefault();
+
+	const title = document.querySelector('#new-post-title').value.trim();
+	const content = document.querySelector('#new-post-content').value.trim();
+
+	const response = await fetch('/api/post', {
+		method: 'POST',
+		body: JSON.stringify({ title, content }),
+		headers: { 'Content-Type': 'application/json' },
+	});
+
+	if (response.ok) {
+		document.location.replace('/dashboard');
+	} else {
+		alert('Failed to add post.');
+	}
+};
+
+document.querySelector('#new-post-button').addEventListener('click', addPost);
